ec642de472cb4b4513e3c54175a41f68e1fe54ee
GonzaloRDuart <68826636+GonzaloRDuart@users.noreply.github.com>
2023-05-26 17:40:27 -0300

    FIX LOCAL

--- INSERT LOCAL.sql
+++ INSERT LOCAL.sql
@@ -1,6 +1,6 @@
 INSERT INTO local_(id_direccion, nombre, descripcion, tipo)
 SELECT DISTINCT d.id_direccion, LOCAL_NOMBRE, LOCAL_DESCRIPCION, t.id_tipo
+FROM gd_esquema.Maestra
 JOIN tipo_local as t ON t.descripcion = LOCAL_TIPO
 JOIN direccion as d ON d.direccion = LOCAL_DIRECCION
-FROM gd_esquema.Maestra
 WHERE LOCAL_NOMBRE IS NOT NULL
